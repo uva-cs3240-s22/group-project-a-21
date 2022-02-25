619cd115098bf5868ab0e2203f418550a9a4ee08
apfoser <alex.pfoser@gmail.com>
2022-02-25 14:23:52 -0500

    requirements_heroku

--- FinalProject/settings.py
+++ FinalProject/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+# Heroku
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Heroku
+django_heroku.settings(locals())
