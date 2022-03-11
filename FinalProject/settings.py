b28a4929d129e923fa6818aeaeb04d667fd437c0
lilizhhh <72361890+lilizhhh@users.noreply.github.com>
2022-03-11 11:33:01 -0500

    added import to settings.py

--- FinalProject/settings.py
+++ FinalProject/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 import django_heroku
 import os
 
+import sys
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
