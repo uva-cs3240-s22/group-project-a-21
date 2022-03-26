4e769597a5de40d14e29d63843b495b559579854
lilizhhh <72361890+lilizhhh@users.noreply.github.com>
2022-03-26 15:45:54 -0400

    put all parts in an accordion (collapsible) and updated the recipe model to include 2 new fields

--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,8 @@
 # from msilib.schema import Directory
+from email.policy import default
 from django.db import models
 from django.contrib import admin
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
@@ -16,7 +18,16 @@ class Recipe(models.Model):
     directionsList = models.CharField(max_length=5000)
     dietaryRestrictions = models.CharField(max_length=200)
     time = models.IntegerField(default=0)
-    servingSize = models.IntegerField(default=1)
+    servingSize = models.PositiveSmallIntegerField(default=1)
+    blurb = models.CharField(max_length=1000, default="no blurb entered")
+    difficultyRating = models.PositiveSmallIntegerField(
+        default=3,
+        validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)
+        ]
+        # https://stackoverflow.com/questions/849142/how-to-limit-the-maximum-value-of-a-numeric-field-in-a-django-model
+    )
 
     def __str__(self):
         return self.title
