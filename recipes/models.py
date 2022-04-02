3763a18ee5b4421773b8a310e51b78bea7979ef5
Thumay Huynh <tvh4xuk@virginia.edu>
2022-04-02 11:31:34 -0400

    adding user model to local branch

--- recipes/models.py
+++ recipes/models.py
@@ -5,7 +5,22 @@ from django.contrib import admin
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+from email.policy import default
+from django.db import models
+from django.contrib import admin
+from django.core.validators import MaxValueValidator, MinValueValidator
 
+# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.CharField(max_length=50)
+    cooking_experience = models.IntegerField()
+    friends = models.ManyToManyField("self")
+    # made_recipes = models.ManyToManyField(Recipe)
+    # favorite_recipes = models.ManyToManyField(Recipe)
+
+    def __str__(self):
+        return self.name
 '''
 Note for future development:
     Separate models for Ingredients and Directions lists
