7ba14901a981c9b760a4cd75ab5e701d0d9a8a57
apfoser <alex.pfoser@gmail.com>
2022-04-08 11:01:53 -0400

    testing user on reviews

--- recipes/models.py
+++ recipes/models.py
@@ -73,6 +73,8 @@ If Recipe is deleted, all related Reviews are deleted as well
 TODO: Incorporate user model so review is linked to user
 '''
 class Review(models.Model):
+    # confused on this (user)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default="none")
     recipe = models.ForeignKey(Recipe, related_name='reviews', on_delete=models.CASCADE);
     review_text = models.TextField(blank=True, null=True)
     rating = models.IntegerField()
