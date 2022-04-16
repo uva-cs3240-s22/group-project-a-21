75e9598d64e0027d6abfd1b243f3b375016664a1
apfoser <alex.pfoser@gmail.com>
2022-04-16 01:23:17 -0400

    register reviews

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Recipe, Profile
+from .models import Recipe, Profile, Review
 # Register your models here.
 
 admin.site.register(Recipe)
 admin.site.register(Profile)
-
+admin.site.register(Review)
