d77db85c0581d2839a09c0c3ebd23abceb7b3b46
lilizhhh <72361890+lilizhhh@users.noreply.github.com>
2022-05-01 15:35:53 -0400

    Added appropriate amount of filled and empty stars in recipe card, recipe page, reviews, profile tabs

--- recipes/templatetags/recipe_tags.py
+++ recipes/templatetags/recipe_tags.py
@@ -34,3 +34,9 @@ def parseInt(number):
     if float(number) == 2.5: # python rounds 2.5 to 2; for our purposes, we want 3
         return 3
     return round(float(number))
+
+@register.filter(name='parseIntFiveMinus') 
+def parseInt(number):
+    if float(number) == 2.5: # python rounds 2.5 to 2; for our purposes, we want 3
+        return 3
+    return 5 - round(float(number))
